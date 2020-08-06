8c8f13d7489f4cba1c43fdf68dc32029cd00189c
yvan poussard <yvs.pro64@gmail.com>
2020-08-06 22:17:23 +0200

    search and litle design

--- app/controllers/propertys_controller.rb
+++ app/controllers/propertys_controller.rb
@@ -1,9 +1,18 @@
 class PropertysController < ApplicationController
     def index
-        @propertys = Property.all
+        if params[:query].present?
+            @propertys = Property.where(name: params[:query])
+          else
+            @propertys = Property.all
+          end
     end
-    
     def show
         @property = Property.find(params[:id])
+    end
+
+    private
+
+    def search
+    params.require(:property).permit(:name)
     end
 end
