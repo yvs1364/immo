fccd1448a2e66cea7f652ae5aaca48496fd6de04
yvan poussard <yvs.pro64@gmail.com>
2020-08-10 20:28:18 +0200

    gem pour heroku

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem "figaro"
 gem 'jquery-rails'
 gem "simple_form"
 
+group :production do
+  gem "pg"
+end
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/javascript/plugins/init_Map.js
+++ app/javascript/plugins/init_Map.js
@@ -1,9 +1,10 @@
 
-let map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+let map: google.maps.Map;
+
+const initMap = () => void {
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 10
   });
 }
-initMap()
+export { initMap };
